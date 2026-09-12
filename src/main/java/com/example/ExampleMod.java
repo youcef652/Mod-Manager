07dba479f40996c2b99b467c74b069c501d5ce29
veosee2 <ybouz2014@gmail.com>
2026-09-12 13:00:00 +0000

    خن

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -21,7 +21,7 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("زميط عاشق ماتنو با با با!");
 	}
 
 	public static Identifier id(String path) {
